301f00167527356d478bba6496e1f4243f2a6269
henklam <183881683+henklam@users.noreply.github.com>
2025-01-30 11:31:31 +0000

    pwa

--- index.js
+++ index.js
@@ -40,3 +40,17 @@ function signIn() {
 }
 
 button.addEventListener("click", signIn);
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('project/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.log('Service Worker registration failed:', error);
+        });
+    });
+  }
+  
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('pwa-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/app.js',
+          '/index.js',
+          '/app.html'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
