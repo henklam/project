8b876f92aff56203270845197cb46c23071b9910
henklam <183881683+henklam@users.noreply.github.com>
2025-01-30 11:58:49 +0000

    c

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open('pwa-cache').then((cache) => {
         return cache.addAll([
-          '/',
           'index.html',
           'app.js',
           'index.js',
