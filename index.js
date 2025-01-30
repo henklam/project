d50eedf3e9d4597d33f646cfbfb66fda01e73366
henklam <183881683+henklam@users.noreply.github.com>
2025-01-30 11:42:07 +0000

    location fix

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ button.addEventListener("click", signIn);
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('project/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then((registration) => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
